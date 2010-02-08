14582359e814b6e30f03abe193f8012f6f8ec870
Denis Martinez <denis@mupuf.org>
2010-02-08 23:32:39 +0100

    editor settings (still TODO)

--- gui/ConfigDialog.cpp
+++ gui/ConfigDialog.cpp
@@ -9,4 +9,14 @@ ConfigDialog::ConfigDialog(QWidget *parent)
     : QxtConfigDialog(parent)
 {
     setWindowTitle(tr("Configuration"));
+    setupUi();
+}
+
+void ConfigDialog::setupUi()
+{
+    QWidget *page;
+
+    page = new QWidget;
+    uiEditor.setupUi(page);
+    addPage(page, QIcon(":/images/32x32/accessories-text-editor.png"), tr("Editor"));
 }
--- gui/ConfigDialog.h
+++ gui/ConfigDialog.h
@@ -8,10 +8,17 @@
 
 #include <qxtconfigdialog.h>
 
+#include "ui_ConfigEditor.h"
+
 class ConfigDialog : public QxtConfigDialog
 {
 public:
     ConfigDialog(QWidget *parent = NULL);
+
+private:
+    void setupUi();
+
+    Ui::ConfigEditor uiEditor;
 };
 
 #endif // CONFIGDIALOG_H
