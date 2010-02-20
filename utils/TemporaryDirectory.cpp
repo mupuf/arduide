3295a66686cc45a67d4263b5cb7f344134822bc3
unknown <mupuf@.(none)>
2010-02-20 05:39:10 +0100

    Windows: Get a temporary filename

--- utils/TemporaryDirectory.cpp
+++ utils/TemporaryDirectory.cpp
@@ -12,13 +12,46 @@
 
 #include "FileUtils.h"
 
-TemporaryDirectory::TemporaryDirectory(const QString &tmpl)
-{
-    QString fullTmpl = QDir::temp().filePath(tmpl + "XXXXXX");
-    QByteArray dir(mkdtemp(fullTmpl.toLocal8Bit().data()));
-    if (! dir.isEmpty())
-        mPath = QString::fromLocal8Bit(dir);
-}
+#if defined(Q_OS_WIN32) || defined(Q_OS_WIN64)
+	#include <windows.h>
+	
+	TemporaryDirectory::TemporaryDirectory(const QString &tmpl)
+	{
+		DWORD dwBufSize=101;
+		TCHAR szTempFileName[dwBufSize];  
+		TCHAR lpTempPathBuffer[dwBufSize];
+
+		 //  Gets the temp path env string (no guarantee it's a valid path).
+		DWORD dwRetVal = GetTempPath(dwBufSize,     // length of the buffer
+							   lpTempPathBuffer); // buffer for path 
+		if (dwRetVal > dwBufSize || (dwRetVal == 0))
+		{
+			fprintf(stderr, "GetTempPath failed");
+			return;
+		}
+
+		//  Generates a temporary file name. 
+		UINT uRetVal = GetTempFileName(lpTempPathBuffer, // directory for tmp files
+								  TEXT(qPrintable(tmpl)),// temp file name prefix 
+								  0,                // create unique name 
+								  szTempFileName);  // buffer for name 
+		if (uRetVal == 0)
+		{
+			fprintf(stderr, "GetTempFileName failed");
+			return;
+		}
+	
+		mPath = QString::fromLocal8Bit(szTempFileName);
+	}
+#else
+	TemporaryDirectory::TemporaryDirectory(const QString &tmpl)
+	{
+		QString fullTmpl = QDir::temp().filePath(tmpl + "XXXXXX");
+		QByteArray dir(mkdtemp(fullTmpl.toLocal8Bit().data()));
+		if (! dir.isEmpty())
+			mPath = QString::fromLocal8Bit(dir);
+	}
+#endif
 
 TemporaryDirectory::~TemporaryDirectory()
 {
