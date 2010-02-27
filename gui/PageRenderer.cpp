a703356c67775a808d896f817466dbda6b5b7269
Martin Peres <martin.peres@ensi-bourges.fr>
2010-02-27 15:20:21 +0100

    Catch the loadByName exception

--- gui/PageRenderer.cpp
+++ gui/PageRenderer.cpp
@@ -7,14 +7,22 @@
 
 #include <QDir>
 #include <engine.h>
+#include <exception.h>
 
 PageRenderer::PageRenderer(const QString &pageName, const QVariantHash &mapping)
 {
-    Grantlee::Engine *engine = Grantlee::Engine::instance();
-    Grantlee::Template t = engine->loadByName(pageName);
-    Grantlee::Context c(mapping);
-
-    mUrl.setScheme("file");
-    mUrl.setPath(QDir(DATA_PATH).filePath(QString("templates/%0").arg(pageName)));
-    mPage = t->render(&c);
+    try
+    {
+        Grantlee::Engine *engine = Grantlee::Engine::instance();
+        Grantlee::Template t = engine->loadByName(pageName);
+        Grantlee::Context c(mapping);
+        
+        mUrl.setScheme("file");
+        mUrl.setPath(QDir(DATA_PATH).filePath(QString("templates/%0").arg(pageName)));
+        mPage = t->render(&c);
+    }
+    catch(Grantlee::Exception e)
+    {
+        qDebug("PageRenderer::PageRenderer: %s", qPrintable(e.what()));
+    }
 }
